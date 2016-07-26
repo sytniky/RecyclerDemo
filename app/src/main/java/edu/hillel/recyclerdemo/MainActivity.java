30533778cc36f30cdc8229fa6e9176c5b510713a
ysytnik <sytniky@gmail.com>
2016-07-26 06:51:13 +0300

    Added item listener

--- app/src/main/java/edu/hillel/recyclerdemo/MainActivity.java
+++ app/src/main/java/edu/hillel/recyclerdemo/MainActivity.java
@@ -1,17 +1,19 @@
 package edu.hillel.recyclerdemo;
 
-import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> data = new ArrayList<>();
+public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener {
+
+    private ArrayList<String> mData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView rv = (RecyclerView) findViewById(R.id.rvRecycler);
 
-        MyAdapter mAdapter = new MyAdapter(data);
+        MyAdapter mAdapter = new MyAdapter(mData, this);
         rv.setAdapter(mAdapter);
 
         LinearLayoutManager lManager = new LinearLayoutManager(this);
@@ -33,8 +35,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        data.add("one");
-        data.add("two");
-        data.add("three");
+        mData.add("one");
+        mData.add("two");
+        mData.add("three");
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
